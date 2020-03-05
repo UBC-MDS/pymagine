bfdfc70d4fdca194325d7220172bb3c78f80d3d7
Trevor <tktrevorkwan@gmail.com>
2020-03-04 18:15:19 -0800

    added edge_detection function and edge_detection test

--- pymagine/edge_detection.py
+++ pymagine/edge_detection.py
@@ -3,11 +3,14 @@ Created on February 26, 2020
 @author: Trevor Kwan
 Implementation of edge_detection function in the pymagine package.
 """
+import numpy as np
+from skimage.color import rgb2gray
+from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
 
-def edge_detection(image, color="Greys"):
-  
-  """
-  Returns the given image with the user-specified
+def edge_detection(filename, color, is_grey = False):
+    """
+    Returns the given image with the user-specified
   edge detection applied.
   
   Parameters
@@ -24,4 +27,16 @@ def edge_detection(image, color="Greys"):
   -------
   image 
   image returned with desired edge detection color filter applied
-  """
+    """
+    filt = np.ones((3,3))/8
+
+    if is_grey:
+        img = plt.imread(filename)
+        I_filt = convolve2d(img,filt, boundary='symm', mode='same')
+
+    else:
+        img = plt.imread(filename)
+        img = rgb2gray(img)
+        I_filt = convolve2d(img,filt, boundary='symm', mode='same')
+        
+    return plt.imshow(I_filt, cmap = color)
