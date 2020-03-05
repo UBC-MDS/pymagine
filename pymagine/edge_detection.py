9516e05a0dda479211a67f5de6d75455e37ff287
Trevor <tktrevorkwan@gmail.com>
2020-03-04 18:34:47 -0800

    added tests for edge detection main function

--- pymagine/edge_detection.py
+++ pymagine/edge_detection.py
@@ -8,7 +8,7 @@ from skimage.color import rgb2gray
 from scipy.signal import convolve2d
 import matplotlib.pyplot as plt
 
-def edge_detection(filename, color, is_grey = False):
+def edge_detection(filename, color = 'Greys', is_grey = False):
     """
     Returns the given image with the user-specified
   edge detection applied.
@@ -28,6 +28,20 @@ def edge_detection(filename, color, is_grey = False):
   image 
   image returned with desired edge detection color filter applied
     """
+    if not isinstance(filename, str):
+        raise TypeError("Image file path must be a string.")
+    
+    if not filename.endswith((".png", ".jpeg", ".jpg")):
+        raise TypeError("Image format must be png, jpg, or jpeg.")
+    
+    if filename.startswith(("https:", "http:", "www.")):
+        raise TypeError("Image file path can't be a URL, provide a local file path.")
+        
+    if color == "Greys" or color == "Purples" or color == "Blues" or color == "Greens" or color == "Oranges" or color == "Reds":
+        True
+    else:
+        raise ValueError("Color can only be one of: 'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds'")
+    
     filt = np.ones((3,3))/8
 
     if is_grey:
