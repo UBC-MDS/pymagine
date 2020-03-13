1cf105c0f8db3dd2c40f6ff6923aee16bfc5e41f
Katie Birchard <birchardkatie@gmail.com>
2020-03-13 11:53:18 -0700

    Fixed minor formatting error

--- pymagine/tunnel_filter.py
+++ pymagine/tunnel_filter.py
@@ -30,7 +30,7 @@ def tunnel_filter(image_path, rot=0.5):
     numpy array
     Altered image array returned for the input tunnel
     filter
-    
+
     Example
     -------
     >>> tunnel_filter("img/picture.jpeg", rot=0.2)
