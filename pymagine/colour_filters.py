eef9d89f5a27ebec103fc1b3e90d609693409d01
Sukriti <sukriti1394@gmail.com>
2020-03-13 10:51:50 -0700

    added tests for output array and edited function script to return array

--- pymagine/colour_filters.py
+++ pymagine/colour_filters.py
@@ -77,12 +77,14 @@ def colour_filters(image, tone="sepia"):
       new_pixel = transformed_blue + transformed_green + transformed_red
       
       gray_image = np.reshape(new_pixel, image_2d.shape)
+      final_image_array = gray_image
       plt.imsave(fname="colour_filter.jpeg", arr=gray_image, cmap="gray")
   
   elif tone.lower() == 'negative':
       image_2d = image[:, :, 0]
       
       negative_image = image_2d.copy()
+      final_image_array = negative_image
       negative_image = 255 - image_2d
       
       plt.imsave(fname="colour_filter.jpeg", arr=negative_image, cmap="gray")
@@ -139,4 +141,5 @@ def colour_filters(image, tone="sepia"):
       
       plt.imsave(fname="colour_filter.jpeg", arr=final_image_array)
   
-  return print("The filtered image has been saved to the working directory")
+  print("The filtered image has been saved to the working directory")
+  return final_image_array
