517bbd0ae853bd5b9c2eafb5f5e6f975e88f263d
Brendon Campbell <brendon.d.campbell@gmail.com>
2020-03-13 11:28:16 -0700

    Added a test for the output image of vignette function and fixed formatting

--- tests/test_vignette.py
+++ tests/test_vignette.py
@@ -1,28 +1,41 @@
-import sys
-import os
 import pytest
-
-import numpy as np
-import pandas as pd
-import cv2
+import os
+import matplotlib.pyplot as plt
 
 from pymagine import vignette_filter as vig
 
-fname = os.path.join(os.path.dirname(__file__), '../tests/imgs/coronado_beach.jpeg')
+fname = os.path.join(
+    os.path.dirname(__file__),
+    '../tests/imgs/coronado_beach.jpeg')
 bad_ftype = 'imgs/coronado_beach.csv'
-url_fname = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg'
+url_fname = 'https://upload.wikimedia.org/wikipedia/Python-logo-notext.svg'
+
 
 def test_inputs():
+    """
+    Applies tests to the vignette function to confirm handling of valid inputs
+    """
     with pytest.raises(TypeError):
-       vig.vignette_filter(True) # Not a string for the file path
+        vig.vignette_filter(True)  # Not a string for the file path
     with pytest.raises(TypeError):
-       vig.vignette_filter(bad_ftype) # Filetype is not image
+        vig.vignette_filter(bad_ftype)  # Filetype is not image
     with pytest.raises(TypeError):
-       vig.vignette_filter(url_fname) # Filepath can't be URL
+        vig.vignette_filter(url_fname)  # Filepath can't be URL
 
     with pytest.raises(ValueError):
-        vig.vignette_filter(fname, strength=-1.0) # Invalid strength value
+        vig.vignette_filter(fname, strength=-1.0)  # Invalid strength value
     with pytest.raises(ValueError):
-        vig.vignette_filter(fname, x=2.0) # Invalid x axis value
+        vig.vignette_filter(fname, x=2.0)  # Invalid x axis value
     with pytest.raises(ValueError):
-        vig.vignette_filter(fname, y=-1.0) # Invalid y axis value
+        vig.vignette_filter(fname, y=-1.0)  # Invalid y axis value
+
+
+def test_outputs():
+    """
+    Applies tests to the function output
+    """
+    test_array = plt.imread(fname)
+
+    # verify output image is the same dimensions as the input image
+    returned_arr_vignette = vig.vignette_filter(fname)
+    assert returned_arr_vignette.shape == test_array[:, :, 0].shape
